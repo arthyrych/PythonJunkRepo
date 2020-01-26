dce17229e102b838cc79d3d484a89e2a7ec506c5
Artem <34424778+arthyrych@users.noreply.github.com>
2020-01-26 15:10:24 +0200

    Update dz.py

--- dz.py
+++ dz.py
@@ -1,3 +1,13 @@
-x = 5
-if x > 4:
- print('Positive')
+print('Homework 2')
+print('Task 1')
+a, b, c, d = 5, 10, 20, 40
+print(a + b)
+print(d / b)
+print(a * c)
+print(d - c)
+print('Task 2')
+x = 'You are '
+y = 'awesome'
+print(x + y)
+print('Task 3')
+
