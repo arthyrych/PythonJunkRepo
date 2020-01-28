5a9c2788755b6bdb2b4588573dfc85783a397fde
Artem <arthyrych@gmail.com>
2020-01-28 13:37:04 +0200

    dz2

--- dz.py
+++ dz.py
@@ -1,14 +1,24 @@
 print('Homework 2')
+
 print('Task 1')
 a, b, c, d = 5, 10, 20, 40
 print(a + b)
 print(d / b)
 print(a * c)
 print(d - c)
+
 print('Task 2')
 x = 'You\'re '
 y = 'awesome'
 z = x + y
 print(z)
-# print('Task 3')
+
+print('Task 3')
+import random
+Start = 9
+Stop = 99
+limit = 10
+randomlist = [random.randint(Start, Stop) for iter in range(limit)]
+del randomlist[-1]
+print(randomlist)
 
