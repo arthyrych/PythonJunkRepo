73302ca0bcd76a8572ee29de680d227ff69a7a75
Artem <34424778+arthyrych@users.noreply.github.com>
2020-01-30 09:38:42 +0200

    Update dz.py

--- dz.py
+++ dz.py
@@ -1,24 +1,8 @@
-print('Homework 2\n')
+print('Homework 3\n')
 
-print('Task 1:')
-a, b, c, d = 5, 10, 20, 40
-print(a + b)
-print(d / b)
-print(a * c)
-print(d - c)
+print('Task 1 with FOR:')
 
-print('\nTask 2:')
-x = 'You\'re '
-y = 'awesome'
-z = x + y
-print(z)
 
-print('\nTask 3:')
-import random
-Start = 9
-Stop = 99
-limit = 10
-list = [random.randint(Start, Stop) for iter in range(limit)]
-print(list)
-del list[-1]
-print(list)
+print('\nTask 2 with WHILE:')
+
+
