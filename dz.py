739b9b0b2ba2b44218313617d9e331ac32c5152e
Artem <34424778+arthyrych@users.noreply.github.com>
2020-01-30 10:39:10 +0200

    Update dz.py

--- dz.py
+++ dz.py
@@ -1,7 +1,23 @@
 print('Homework 3\n')
 
 print('Task 1 with FOR:')
-
+print('\nEnter A, B, C each one from the new line and press ENTER')
+A = input()
+B = input()
+C = input()
+print(A, B, C)
+if A > B:
+    print('Svershilos!')
+elif B > A:
+    print('Da nu!')
+elif A == B:
+    print('A esli tak?')
+    A = int(A) + int(C)
+    B = int(B) - int(C)
+    if A > B:
+     print('Svershilos!')
+    elif B > A:
+     print('Da nu!')
 
 print('\nTask 2 with WHILE:')
 
