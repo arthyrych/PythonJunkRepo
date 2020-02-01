4ca0fa59e5b2af007abfcaf8cd6ba43431a8816f
Artem <arthyrych@gmail.com>
2020-02-01 18:11:56 +0200

    changes

--- dz.py
+++ dz.py
@@ -1,24 +1,18 @@
-print('Homework 2\n')
-
-print('Task 1:')
-a, b, c, d = 5, 10, 20, 40
-print(a + b)
-print(d / b)
-print(a * c)
-print(d - c)
-
-print('\nTask 2:')
-x = 'You\'re '
-y = 'awesome'
-z = x + y
-print(z)
-
-print('\nTask 3:')
-import random
-Start = 9
-Stop = 99
-limit = 10
-list = [random.randint(Start, Stop) for iter in range(limit)]
-print(list)
-del list[-1]
-print(list)
+print('Fill in A, B, C (each one from the new line and press Enter)')
+A = input()
+B = input()
+C = input()
+print(A, B, C)
+if A > B:
+    print('Svershilos!')
+    if B > A:
+        print('Da nu!')
+        if A == B:
+            print('A esli tak?')
+            x = A + C
+            y = B - C
+            if x > y:
+                print('Svershilos!')
+                if y > x:
+                    print('Da nu!')
+print('Happy end!')
