f16f8904024cbf417b0f29b4b1a10c22cba839ca
Artem <arthyrych@gmail.com>
2020-02-03 17:09:01 +0200

    changed

--- dz(functions).py
+++ dz(functions).py
@@ -1,1 +1,18 @@
 print('Homework 4\n')
+
+print('Function 1\n')
+thislist = []
+
+
+def mylist(x, y):
+    print(x, y)
+    for x in range(x):
+        thislist.append(x * 1)
+        if x > y:
+            thislist.pop()
+    print(thislist)
+
+
+mylist(10, 7)
+
+print('Function 2\n')
