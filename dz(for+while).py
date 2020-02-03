90b31524994c55732cb41ae35a0d47c7ce0ff64a
Artem <arthyrych@gmail.com>
2020-02-03 16:27:38 +0200

    changes

--- dz(for+while).py
+++ dz(for+while).py
@@ -1,24 +1,5 @@
 print('Homework 3\n')
-
-print('Task 1 with FOR:')
-print('\nEnter A, B, C each one from the new line and press ENTER')
-A = input()
-B = input()
-C = input()
-print(A, B, C)
-if int(A) > int(B):
-    print('Svershilos!')
-elif int(B) > int(A):
-    print('Da nu!')
-elif int(A) == int(B):
-    print('A esli tak?')
-    A = int(A) + int(C)
-    B = int(B) - int(C)
-    if int(A) > int(B):
-     print('Svershilos!')
-    elif int(B) > int(A):
-     print('Da nu!')
-
+# print('Task 1 with FOR:')
 print('\nTask 2 with WHILE:')
 print('\nEnter A, B, C each one from the new line and press ENTER')
 A = input()
