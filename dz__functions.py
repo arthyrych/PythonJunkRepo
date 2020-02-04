4a92ead095e72782557ac389816b498acf8e904a
Artem <arthyrych@gmail.com>
2020-02-04 14:01:00 +0200

    changed

--- dz__functions.py
+++ dz__functions.py
@@ -3,98 +3,90 @@ print('Homework 4\n')
 print('Function 1\n')
 # Необходимо создать функцию, которая будет принимать два аргумента от
 # пользователя и на основании этих аргументов создавать список.
-#
 # Первый аргумент - определяет длину списка
 # Второй аргумент - определяет максимально возможное значение элемента списка
 
+user_list = []
+LENGTH_USER_LIST = 'Enter the length of the list: '
+LENGTH_USER_LIST_ARG = 'Enter the MAX value of an arg in the list: '
+
 
 def set_user_list(list_length, arg_length):
-    print("--------------------------------------")
-
     for i in range(list_length):
-        print("Введите значение ", i, " елемента: ")
+        print("Enter the value of ", i, " element: ")
         while True:
             arg = input()
             if not arg:
-                print("Ошибка. Введите число от ", 0, " до ", arg_length)
+                print("Error. Enter the value from ", 0, " to ", arg_length)
                 continue
             if int(arg) > arg_length:
-                print("Ошибка. Введите число от ", 0, " до ", arg_length)
+                print("Error. Enter the value from ", 0, " to ", arg_length)
                 continue
             else:
                 user_list.append(arg)
-                print("OK")
+                print("Accepted!")
                 break
 
-    print("--------------------------------------")
-
-
-user_list = []
-LENGTH_USER_LIST = 'Введите длину списка: '
-LENGTH_USER_LIST_ARG = 'Введите максимальное значение элемента списка: '
 
 set_user_list(int(input(LENGTH_USER_LIST)), int(input(LENGTH_USER_LIST_ARG)))
+print(user_list)
 
-
+print('\nFunction 2\n')
 # Необходимо написать функцию, которая будет принимать два аргумента по тому
-# # же принципу что и в предыдущей
+# же принципу что и в предыдущей
 # после чего анализировать список и выводить в консоль все элементы списка,
 # которые больше 7.
-
-
-def set_user_list(list_length, arg_length):
-    print("--------------------------------------")
-
-    for i in range(list_length):
-        print("Введите значение ", i, " елемента: ")
-        while True:
-            arg = input()
-            if not arg:
-                print("Ошибка. Введите число от ", 0, " до ", arg_length)
-                continue
-            if int(arg) > arg_length:
-                print("Ошибка. Введите число от ", 0, " до ", arg_length)
-                continue
-            else:
-                user_list.append(arg)
-                print("OK")
-                break
-
-    print("--------------------------------------")
-    print("Анализируем список и выводим элементы списка, которые больше 7:")
-
-    for i in range(len(user_list)):
-        if int(user_list[i]) > 7:
-            print("Элемент списка ", i, " имеет значение ", user_list[i], " больше 7")
-
-    print("--------------------------------------")
-
 
 user_list = []
 user_list_length = 0
 user_list_arg_length = 0
 
+
+def set_user_list(list_length, arg_length):
+    for i in range(list_length):
+        print("Enter the value of ", i, " element: ")
+        while True:
+            arg = input()
+            if not arg:
+                print("Error. Enter the value from ", 0, " to ", arg_length)
+                continue
+            if int(arg) > arg_length:
+                print("Error. Enter the value from ", 0, " to ", arg_length)
+                continue
+            else:
+                user_list.append(arg)
+                print("Accepted!")
+                break
+
+    print("Checking the received data and giving back the ones which are bigger than 7: ")
+
+    for i in range(len(user_list)):
+        if int(user_list[i]) > 7:
+            print("The element of the list ", i, " is having value ", user_list[i], " and bigger than 7")
+
+
 while True:
-    user_list_length = int(input("Введите длину списка: "))
+    user_list_length = int(input("Enter the length of the list: "))
     if not user_list_length:
-        print("Введите значение > 0")
+        print("Enter the value > 0")
         continue
     elif user_list_length > 0:
-        print("OK")
+        print("Accepted!")
         break
     else:
         continue
 
 while True:
-    user_list_arg_length = int(input("Введите максимальное значение елемента: "))
+    user_list_arg_length = int(input("Enter the MAX arg of the element in the list: "))
     if not user_list_arg_length:
-        print("Введите значение > 0")
+        print("Enter the value > 0")
         continue
     elif user_list_length > 0:
-        print("OK")
+        print("Accepted!")
         break
     else:
         continue
 
 set_user_list(user_list_length, user_list_arg_length)
 
+print('\nFunction 3\n')
