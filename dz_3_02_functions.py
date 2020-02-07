1ea9c61f9f82484b6965ab6cc90491f0e1042bc0
Artem <arthyrych@gmail.com>
2020-02-07 10:39:25 +0200

    changed

--- dz_3_02_functions.py
+++ dz_3_02_functions.py
@@ -4,6 +4,24 @@ print('Function 1\n')
 # Необходимо создать три словаря и написать функцию,
 # которая сможет брать словари и производить их слияние в один
 
+car_1 = {"model": "Model S", "name": 'Tesla', "year": 2017}
+car_2 = {"model": "Leaf", "name": 'Nissan', "year": 2019}
+car_3 = {"model": "Zoe", "name": 'Renault', "year": 2020}
+my_cars = {}
+
+
+def my_cars_fun():
+    my_cars_add = {
+        "car_1": car_1,
+        "car_2": car_2,
+        "car_3": car_3
+    }
+    my_cars.update(my_cars_add)
+
+
+print(car_1, '\n', car_2, '\n', car_3)
+my_cars_fun()
+print('\n', my_cars)
 
 print('Function 2\n')
 # Нужно написать функцию, которая позволит Вам конвертировать
