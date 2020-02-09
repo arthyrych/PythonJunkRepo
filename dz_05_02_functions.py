3459b24850418b3dfc980a7137b0649e928a9461
Artem <arthyrych@gmail.com>
2020-02-09 23:39:49 +0200

    added

--- dz_05_02_functions.py
+++ dz_05_02_functions.py
@@ -18,9 +18,20 @@ def first_last():
 
 
 first_last()
-print(my_list)
-print(my_list[0], my_list[-1])
+print('You\'ve created this list:', my_list)
+print('The first value in the list is', my_list[0], 'and the last value in the list is', my_list[-1])
 
-print('Function 2\n')
-print('Function 3\n')
-print('Function 4\n')
+print('\nFunction 2\n')
+# Найдите три ключа с самыми высокими значениями в словаре
+# my_dict = {'a':645, 'b':3987, 'c': 093,'d':111, 'e':646, 'f': 20}
+
+print('\nFunction 3\n')
+# Создать функцию, которая прибавляет элементы числа между собой.
+# Например, есть число 123. Попав в нашу функцию, должно произойти следующее:
+# 1 + 2 + 3 = 6.
+# Результат суммирования - в консоль.
+
+print('\nFunction 4\n')
+# Нужно написать функцию, которая будет считать сколько раз символ встречается в строке.
+# Например, в строке "Денис даёт нам классные задачки, которые помогут нам найти лучшую работу!"
+# символ "е" встречается 3 раза.
