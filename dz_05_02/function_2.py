a33553c9913088a35396532f9e32f0e7d6b27ed8
Artem <34424778+arthyrych@users.noreply.github.com>
2020-02-10 18:07:33 +0200

    Update function_2.py

--- dz_05_02/function_2.py
+++ dz_05_02/function_2.py
@@ -3,3 +3,56 @@ print('Homework 6\n')
 print('\nFunction 2\n')
 # Найдите три ключа с самыми высокими значениями в словаре
 # my_dict = {'a':645, 'b':3987, 'c': 093,'d':111, 'e':646, 'f': 20}
+my_dict = {'a': 645, 'b': 3987, 'c': 93,'d': 111, 'e': 646, 'f': 20}
+x = my_dict['a']
+for key, value in my_dict.items():
+    if value > x:
+        x = value
+y = my_dict['a']
+for key, value in my_dict.items():
+    if value > y and value < x:
+        y = value
+z = my_dict['a']
+for key, value in my_dict.items():
+    if value > z and value < x and value < y:
+        z = value
+print(x, y, z)
+
+# Найдите три ключа с самыми высокими значениями в словаре
+my_dict = {'a': 645, 'b': 3987, 'c': 93, 'd': 111, 'e': 646, 'f': 20}
+
+new_my_dict = []
+
+for a_index in range(3):
+    biggest_number = 0
+
+    for b_value in my_dict.values():
+        if b_value in new_my_dict:
+            continue
+        if b_value > biggest_number:
+            biggest_number = b_value
+
+    new_my_dict.append(biggest_number)
+
+for g in new_my_dict:
+    print(g)
+    
+# Найдите три ключа с самыми высокими значениями в словаре
+my_dict = {'a': 645, 'b': 3987, 'c': 93, 'd': 111, 'e': 646, 'f': 20}
+
+new_my_dict = {}
+
+for a_index in range(3):
+    biggest_key = ''
+    biggest_value = 0
+    for b_key in my_dict.keys():
+        if b_key in new_my_dict:
+            continue
+        if my_dict[b_key] > biggest_value:
+            biggest_key = b_key
+            biggest_value = my_dict[b_key]
+
+    new_my_dict[biggest_key] = biggest_value
+
+for g in new_my_dict:
+    print(g, new_my_dict[g])
