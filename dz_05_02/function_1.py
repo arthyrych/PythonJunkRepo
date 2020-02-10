f9beabd0bdd17a75d6150bea6afe90ee2a7ff955
Artem <34424778+arthyrych@users.noreply.github.com>
2020-02-10 14:31:04 +0200

    Update function_1.py

--- dz_05_02/function_1.py
+++ dz_05_02/function_1.py
@@ -1,1 +1,22 @@
+print('Homework 6\n')
 
+print('Function 1\n')
+# Создать функцию, которая:
+# Принимает последовательность чисел
+# Превращает ее в список
+# Выводит первый и последний элемент списка
+my_list = []
+
+
+def first_last():
+    while True:
+        x = input('Enter a number to add to the list or enter EXIT to exit:\n')
+        if x == 'EXIT':
+            break
+        else:
+            my_list.append(x)
+
+
+first_last()
+print('You\'ve created this list:', my_list)
+print('The first value in the list is', my_list[0], 'and the last value in the list is', my_list[-1])
