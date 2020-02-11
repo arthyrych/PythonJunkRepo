23415ba98d27dac079297d9cdfbc3a5938d0fb41
Artem <arthyrych@gmail.com>
2020-02-11 14:39:20 +0200

    changed

--- dz_05_02/function_4.py
+++ dz_05_02/function_4.py
@@ -3,3 +3,17 @@ print('\nFunction 4\n')
 # Нужно написать функцию, которая будет считать сколько раз символ встречается в строке.
 # Например, в строке "Денис даёт нам классные задачки, которые помогут нам найти лучшую работу!"
 # символ "е" встречается 3 раза.
+
+
+def amount(our_str):
+    y = 0
+    for char in our_str:
+        if char == str('a'):
+            y += 1
+    if y > 0:
+        print('The sum of \'a\' in string', '\'', our_str, '\'', 'is', y)
+    else:
+        print('No \'a\' in string \'', our_str, '\'')
+
+
+amount(input('Enter a string to sum \'a\' symbols in it: '))
