7bf9d253a7005cca8b0d0508c58655f308c40eef
Artem <arthyrych@gmail.com>
2020-03-02 14:05:45 +0200

    added

--- test.py
+++ test.py
@@ -1,52 +1,51 @@
 # 1) Напишите программу, вычисляющую среднее арифметическое двух чисел.
-# 2) Введите три числа и выведите на экран значения суммы и произведения этих чисел.
-# 3) Напишите простой конвертер валют (без возможности динамического выбора валют пользователем).
-# Валюты заданы хардкодом и не изменяются. Тип валют на выбор программиста.
-# тут обычная арифметика но можешь задать это что бы каждое задание это было новая функция
-print('Task 1. Getting an average.')
 
 
-def average(first, second):
+def average():
+    print('Task 1. Getting an average.')
+    first = int(input('Enter the first number: '))
+    second = int(input('Enter the second number: '))
     print('The average from', first, 'and', second, 'is', (first + second) / 2)
 
 
-average(int(input('Enter the first number: ')), int(input('Enter the second number: ')))
+average()
 
-print('\nTask 2. Getting sum and multiplication.')
-
-
-def user_sum(first, second, third):
-    print('The sum from', first, 'and', second, 'and', third, 'is', first + second + third, '\n')
+# 2) Введите три числа и выведите на экран значения суммы и произведения этих чисел.
 
 
-def multiplication(first, second, third):
-    print('The multiplication from', first, 'and', second, 'and', third, 'is', first * second * third)
+def user_sum():
+    print('\nTask 2. Getting sum and multiplication.')
+    first = int(input('Enter the first number: '))
+    second = int(input('Enter the second number: '))
+    third = int(input('Enter the third number: '))
+    print('The sum is', first, '+', second, '+', third, '=', first + second + third,)
+    print('The multiplication is', first, '*', second, '*', third, '=', first * second * third)
 
 
-user_sum(int(input('Enter the first number: ')),
-         int(input('Enter the second number: ')),
-         int(input('Enter the third number: ')))
-multiplication(int(input('Enter the first number: ')),
-               int(input('Enter the second number: ')),
-               int(input('Enter the third number: ')))
+user_sum()
 
-print('\nTask 3. Money exchange UAH to USD.')
+# 3) Напишите простой конвертер валют (без возможности динамического выбора валют пользователем).
+# Валюты заданы хардкодом и не изменяются. Тип валют на выбор программиста.
+# тут обычная арифметика но можешь задать это что бы каждое задание это было новая функция
 
 
-def kapusta(bablo):
-    print(bablo, 'in USD (US dollars) will be', bablo / 24.1)
+def money():
+    print('\nTask 3. Money exchange UAH to USD.')
+    uah = int(input('Enter the amount in UAH (Ukrainian Hryvnya): '))
+    usd = round(uah / 24.1, 2)
+    print(uah, 'UAH in USD (US dollars) will be', usd)
 
 
-kapusta(int(input('Enter the amount in UAH (Ukrainian Hryvnya): ')))
+money()
 
 # Сделай функцию которая вызывает у себя в теле бесконечный цикл в котором пользователь
 # вводит число и это число проверяется на четность и сообщает об этом пользователю при
 # этом что бы программа не закрывалась, а закрывалась только тогда когда при вводе числа ты как бы ввел пустую
 # строку(просто нажав Enter)
-print('\nTask 4. Is your number even or odd?')
 
 
-def pizdos():
+def even_or_odd():
+    print('\nTask 4. Is your number even or odd?')
     while True:
         x = input('Enter the number or press ENTER to close it: ')
         if not x:
@@ -58,4 +57,4 @@ def pizdos():
             print(x, 'is an odd number.')
 
 
-pizdos()
+even_or_odd()
