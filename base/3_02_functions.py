11a2b06cfc0d2a7041a9c594ffd0ed6dfe634539
Artem <arthyrych@gmail.com>
2020-03-03 11:57:29 +0200

    changed

--- base/3_02_functions.py
+++ base/3_02_functions.py
@@ -1,32 +1,32 @@
-print('Homework 5\n')
-
-print('Function 1\n')
 # Необходимо создать три словаря и написать функцию,
 # которая сможет брать словари и производить их слияние в один
 
-car_1 = {"model": "Model S", "name": 'Tesla', "year": 2017}
-car_2 = {"model": "Leaf", "name": 'Nissan', "year": 2019}
-car_3 = {"model": "Zoe", "name": 'Renault', "year": 2020}
-my_cars = {}
-
 
 def my_cars_fun():
+    print('Function 1')
+    car_1 = {"model": "Model S", "name": 'Tesla', "year": 2017}
+    car_2 = {"model": "Leaf", "name": 'Nissan', "year": 2019}
+    car_3 = {"model": "Zoe", "name": 'Renault', "year": 2020}
+    print(car_1, '\n', car_2, '\n', car_3)
+    my_cars = {}
     my_cars_add = {
         "car_1": car_1,
         "car_2": car_2,
         "car_3": car_3
     }
     my_cars.update(my_cars_add)
+    print('My cars:', my_cars)
 
 
-print(car_1, '\n', car_2, '\n', car_3)
 my_cars_fun()
-print('\n', my_cars)
 
-print('Function 2\n')
 # Нужно написать функцию, которая позволит Вам конвертировать
 # указанное Вами число секунд в формат записи дни:часы:минуты:секунды
-def time(x):
+
+
+def time():
+    print('Function 2')
+    x = int(input('Enter the amount of seconds which will be converted into dd/hh/mm/ss: '))
     dd = x // 86400
     ost = x % 86400
     hh = ost // 3600
@@ -34,20 +34,21 @@ def time(x):
     mm = ost // 60
     ost = ost % 60
     ss = ost
-    print('Time is', dd, ':', hh, ':', mm, ':', ss)
+    print('Time is', dd, 'days', hh, 'hours', mm, 'minutes', 'and', ss, 'seconds')
     
     
-time (int(input('Enter the amount of seconds which will be converted into dd/hh/mm/ss:')))
+time()
 
-print('\nFunction 3 and it is the same as the previous one\n')
 # Нужно написать функцию, которая позволит Вам конвертировать
 # указанное Вами число секунд в формат записи дни:часы:минуты:секунды
 from datetime import timedelta
 
 
-def convert(input_second):
-    d = timedelta(seconds=input_second)
+def convert():
+    print('Function 3 and it is the same as the previous one: ')
+    x = int(input("Enter seconds: "))
+    d = timedelta(seconds=x)
     print(d)
 
 
-convert(int(input("Enter seconds: ")))
+convert()
